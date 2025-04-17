87fb8c6baba1b31b87daf40217a3cdfe686d2fee
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-04-17 17:53:03 +0100

    Add functions for playing rock paper scissors
    This commit adds functions for playing rock paper scissors. It includes
    a function to randomly generate a computer choice, a function to prompt
    the user for their choice, variables for tracking score, and a function
    for playing a single round (I probably should have committed them one
    at a time, but I locked in too hard - oops). This commit is equivalent
    to steps 1-5 of the Rock Paper Scissors project from The Odin Project.

---
+++ scripts.js
@@ -0,0 +1,67 @@
+//declare variables in global scope
+let computerScore = 0;
+let humanScore = 0;
+
+//get computer to return 'rock' 'paper' or 'scissors'
+function getComputerChoice() {
+    //use math.random and multiply by 3 to get a random integer that is either 0, 1, or 2
+    let computerVar = Math.floor(Math.random() * 3);
+    //assign 0 to rock
+    if (computerVar === 0) {
+        return 'rock';
+    //assign 1 to paper
+    } else if (computerVar === 1) {
+        return 'paper';
+    //assign 2 to scissors
+    } else if (computerVar === 2) {
+        return ('scissors')
+    }
+}
+
+//create function getHumanChoice
+function getHumanChoice() {
+    //prompt user requesting what their choice is and store it in humanChoice
+    let humanVar = prompt('rock, paper, scissors!');
+    //convert humanVar to lowercase
+    humanVar = humanVar.toLowerCase();
+    return humanVar;
+}
+
+//run both choice functions to acquire selection paramaters
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+//create function to play round
+function playRound(computerChoice, humanChoice) {
+    //run getComputerChoice to get its return value
+    computerChoice = computerSelection;
+    //run getHumanChoice to get its return value
+    humanChoice = humanSelection;
+    //compare computerChoice with humanChoice to see if human wins
+    if ((computerChoice === 'rock' && humanChoice === 'paper') ||
+    (computerChoice === 'paper' && humanChoice === 'scissors') ||
+    (computerChoice === 'scissors' && humanChoice === 'rock')) {
+        //if human wins, increment humanScore
+        humanScore = humanScore++;
+        //display victory message
+        console.log(`You win: ${humanChoice} beats ${computerChoice}!`);
+    //compare computerChoice with humanChoice to see if computer wins
+    } else if ((computerChoice === 'rock' && humanChoice === 'scissors') ||
+    (computerChoice === 'paper' && humanChoice === 'rock') ||
+    (computerChoice === 'scissors' && humanChoice === 'paper')) {
+        //if computer wins, increment computerScore
+        computerScore = computerScore++;
+        //display defeat message
+        console.log(`You lose: ${computerChoice} beats ${humanChoice}.`);
+        //compare computerChoice with humanChoice to see if they are the same
+    } else if (computerChoice === humanChoice) {
+        //if it's tie, no one's score gets incremented; display tie message
+        console.log(`It's a tie! You both chose ${computerChoice}.`);
+        //if none of the above were true, then something went wrong - display error message
+    } else {
+        console.log('Something went wrong.')
+    }
+}
+
+//run playRound function with acquired parameters
+playRound(computerSelection, humanSelection);
